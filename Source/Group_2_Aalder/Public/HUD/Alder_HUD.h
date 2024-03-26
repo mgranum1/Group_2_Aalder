aafe81266cbf46bd17e058c516fecbc59cc7d067
Håvard_Jensen <142876336+Haavard269408@users.noreply.github.com>
2024-03-26 15:18:30 +0100

    Made main_HUD class (AlderHUDoverlay)

---
+++ Source/Group_2_Aalder/Public/HUD/Alder_HUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "Alder_HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GROUP_2_AALDER_API AAlder_HUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
