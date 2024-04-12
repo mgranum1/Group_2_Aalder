ed7cbfc10ff4c5ec7c8b0dc3274342adc85435bb
Håvard_Jensen <142876336+Haavard269408@users.noreply.github.com>
2024-04-12 15:23:18 +0200

    EnemyAI_Interface

---
+++ Source/Group_2_Aalder/Public/Interfaces/EnemyAI_Interface.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EnemyAI_Interface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UEnemyAI_Interface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class GROUP_2_AALDER_API IEnemyAI_Interface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
